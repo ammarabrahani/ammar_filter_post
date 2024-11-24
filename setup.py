3cb8c8c3543323e8ae56676c15ca7ebbd5733013
ammarabrahani <ammarabrahani@gmail.com>
2024-11-24 19:09:03 +0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='A package to retrieve and sort DynamoDB posts by likes.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/ammarabrahani@gmail.com/ammar_filter_post',
+    url='https://github.com/ammarabrahani/ammar_filter_post',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
